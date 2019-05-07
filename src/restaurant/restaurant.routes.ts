c605f5054875c157f168b1e6df0413b6fded6ba3
shukla <aayush.shukla@capgemini.com>
2019-05-07 15:53:57 +0530

    added files

---
+++ src/restaurant/restaurant.routes.ts
@@ -0,0 +1,14 @@
+import { Routes } from "@angular/router";
+import { RestaurantLoginComponent } from './restaurantLogin.component';
+import { RestaurantRegistrationComponent } from './restaurantRegistration.component';
+import { RestaurantApproveComponent } from './restaurantApprove.component';
+import { WelcomeComponent } from 'src/app/welcome.component';
+
+
+export const resturantRoutes: Routes = [
+    {path:"resturant/login",component: RestaurantLoginComponent},
+    {path:"resturant/signup",component: RestaurantRegistrationComponent},
+    {path:"resturant/approval",component: RestaurantApproveComponent},
+    {path:"resturant-loggedin",component: WelcomeComponent}
+
+];
---
+++ src/restaurant/restaurantLogin.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+    templateUrl:"./restaurantLogin.component.html",
+    // selector:"customer-login"
+    styleUrls: ["./restaurantLogin.component.css"]
+
+
+})
+export class RestaurantLoginComponent {
+    
+    
+}
